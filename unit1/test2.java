867a42992f9f5fa7f2b4b36c27b256921b238288
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-04-28 12:06:23 +0530

    Add files via upload

---
+++ unit1/test2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class test2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char character = sc.next().charAt(0);
+        int ascii = character;
+        System.out.println(ascii);
+    }
+}
